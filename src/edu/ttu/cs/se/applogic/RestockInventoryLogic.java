cc63c8f9c88196a4d25cc61dceb569fdad1472f2
rkelley20 <ryankelley95@gmail.com>
2018-11-14 22:51:46 -0600

    Added various functions to restocker subsystem

--- src/edu/ttu/cs/se/applogic/RestockInventoryLogic.java
+++ src/edu/ttu/cs/se/applogic/RestockInventoryLogic.java
@@ -1,14 +1,20 @@
 package edu.ttu.cs.se.applogic;
 
+import edu.ttu.cs.se.entity.InventoryEntity;
+
 public class RestockInventoryLogic
 {
+    // Checks to see if the item name is an item in inventory, and updates it if it is.
     public static void processRestock(String name, int quantity)
     {
-
+        if (checkItem(name))
+        {
+            InventoryEntity.addItems(name, quantity);
+        }
     }
 
-    private boolean checkItem(String name)
+    private static boolean checkItem(String name)
     {
-
+        return InventoryEntity.getItem(name) != null;
     }
 }
