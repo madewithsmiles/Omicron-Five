4228987ca7d4f6f5b73eb28641f03a503e0c3ab2
madewithsmiles <jndiarrassouba@gmail.com>
2018-11-14 21:46:58 -0600

    ItemEntity work
    added private attributes and getters and setters

--- src/edu/ttu/cs/se/entity/ItemEntity.java
+++ src/edu/ttu/cs/se/entity/ItemEntity.java
@@ -1,4 +1,49 @@
 package edu.ttu.cs.se.entity;
 
 public class ItemEntity {
+    private String name;
+    private Double price;
+    private String desc;
+    private Boolean alcohol;
+    private Double discount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Boolean getAlcohol() {
+        return alcohol;
+    }
+
+    public void setAlcohol(Boolean alcohol) {
+        this.alcohol = alcohol;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
 }
