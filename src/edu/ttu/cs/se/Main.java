61a39d8eba4a703f2d7a420eaa2c2604c5a7b98f
rkelley20 <ryankelley95@gmail.com>
2018-11-16 19:01:03 -0600

    Removed .idea files, added main menu logic

--- src/edu/ttu/cs/se/Main.java
+++ src/edu/ttu/cs/se/Main.java
@@ -1,26 +1,78 @@
 package edu.ttu.cs.se;
 
+import edu.ttu.cs.se.applogic.IOHelper;
 import edu.ttu.cs.se.entity.InventoryEntity;
+import edu.ttu.cs.se.uinterface.CheckoutUI;
 
 public class Main {
+
     private static InventoryEntity inventory;
+    private static CheckoutUI checkout;
 
-    private static void initSession() {
-        inventory = new InventoryEntity();
-        //Simple Test
-        //String test = "Soft Cremes";
-        //ItemEntity test_result = inventory.getItem(test);
-        //System.out.println("Name: " + test_result.getName());
-        //System.out.println("Price: " + test_result.getPrice());
-        //System.out.println("Description: " + test_result.getDesc());
-        //System.out.println("Alcohol: " + test_result.getAlcohol());
-        //System.out.println("Quantity: " + inventory.getQuantity(test));
-    }
-
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
         initSession();
+        run();
     }
+
+    private static void run() {
+        System.out.println("Self checkout main menu");
+        System.out.println("1) Self checkout");
+        System.out.println("2) Manager");
+        System.out.println("3) Restocker");
+        int choice = Integer.parseInt(IOHelper.getInputString("Choice"));
+
+        switch (choice) {
+            case 1:
+                checkoutMenu();
+                break;
+            case 2:
+                managerMenu();
+                break;
+            case 3:
+                restockerMenu();
+                break;
+        }
+
+    }
+
+    private static void initSession()
+    {
+        inventory = new InventoryEntity();
+        checkout = new CheckoutUI();
+    }
+
+    private static void restockerMenu() {
+
+    }
+
+    private static void checkoutMenu() {
+        checkout.display(CheckoutUI.DisplayCode.WELCOME);
+        checkout.startCheckout();
+        while (true) {
+            checkout.display(CheckoutUI.DisplayCode.NORMAL);
+            int input = IOHelper.getInputInt("Choice");
+            boolean done = false;
+            switch (input) {
+                case 1:
+                    checkout.scanItem();
+                    break;
+                case 2:
+                    checkout.total();
+                    done = true;
+                    break;
+            }
+            if (done) {
+                break;
+            }
+        }
+        checkoutMenu();
+    }
+
+    private static void managerMenu() {
+
+    }
+
 
 
 }
